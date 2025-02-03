ff65b527b732992876bc1149b3603df44d76be1e
JunaidAlice <maskedwolf211@gmail.com>
2025-02-03 22:49:03 +0500

    Refactor ticket pricing logic and add photo option prompt in rollercoaster ticketing system

--- Projects/6.Rokkercoster/Ticket.py
+++ Projects/6.Rokkercoster/Ticket.py
@@ -1,15 +1,24 @@
+bill=0
+
 print("Hello Sir, welcome to the rollercoster!")
 hight=float(input("Please enter your hight:"))
 if hight>=120:
   print("You can ride Sir!")
   age=int(input("Please enter your age:"))
-  if age>=18:
+ 
+  if age>=18: 
+    bill=5
     print('Plz pay $5 💵')
   elif   age>18:
+    bill=7
     print("pay $7 💵")
   elif age>12:
+    bill=12
     print("pay $12 💵")
   else:
-    print("you are not allowed to ride 😒 ") 
-else:
+    print("you are not allowed to ride 😒 ")
+  
+  wants_photo=input("do you want photo? y or N")
+  if wants_photo == "y":
+    else:
     print("Sorry you can't ride")
