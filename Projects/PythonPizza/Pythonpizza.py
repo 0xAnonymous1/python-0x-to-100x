125735032bab078ba7da278084fd4046823a0f3e
JunaidAlice <maskedwolf211@gmail.com>
2025-02-11 14:51:20 +0500

    Validate pizza size input and improve conditional logic for bill calculation in pizza ordering system

--- Projects/PythonPizza/Pythonpizza.py
+++ Projects/PythonPizza/Pythonpizza.py
@@ -7,27 +7,30 @@ bill=0
 print("Welcome to python pizza 😊")
 print("Alert: Plz input capital latters👍")
 pizza_size = input("what size pizza do you want? S, M, L :")
-add_pepperroni= input("do you want pepproni?(Y ,N):")
-if pizza_size =="S" and add_pepperroni == "Y":
-  bill=small_pizza+2
-if pizza_size =="S" and add_pepperroni == "N":
-  bill=small_pizza
-if pizza_size=="M" and add_pepperroni == "Y":
-  bill=medium_pizza+2
-if pizza_size=="M" and add_pepperroni == "N":
-  bill=medium_pizza
-if pizza_size =="L" and add_pepperroni == "Y":
-  bill=large_pizza+3
-if pizza_size =="L" and add_pepperroni == "N":
-  bill=large_pizza
-extra_cheese = input("do you want some extra cheese (Y,N):")
-if extra_cheese=="Y":
-  bill+=1
-  print(f"your bill is ${bill}")
-if extra_cheese=="N":
-  bill
-  print(f"your bill is ${bill}")    
+if pizza_size=="S" or  pizza_size=="M" or  pizza_size=="L":
+  add_pepperroni= input("do you want pepproni?(Y ,N):")
+  if pizza_size =="S" and add_pepperroni == "Y":
+    bill=small_pizza+2
+  if pizza_size =="S" and add_pepperroni == "N":
+    bill=small_pizza
+  if pizza_size=="M" and add_pepperroni == "Y":
+    bill=medium_pizza+2
+  if pizza_size=="M" and add_pepperroni == "N":
+    bill=medium_pizza
+  if pizza_size =="L" and add_pepperroni == "Y":
+    bill=large_pizza+3
+  if pizza_size =="L" and add_pepperroni == "N":
+    bill=large_pizza
+  extra_cheese = input("do you want some extra cheese (Y,N):")
+  if extra_cheese=="Y":
+    bill+=1
+    print(f"your bill is ${bill}")
+  if extra_cheese=="N":
+    bill
+    print(f"your bill is ${bill}")    
 
+else:
+  print("your input is invalid 😒")
   
   
 
