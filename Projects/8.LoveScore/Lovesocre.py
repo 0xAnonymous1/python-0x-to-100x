54c44dc9ba7303b449047f0465216a261fc7737e
JunaidAlice <maskedwolf211@gmail.com>
2025-02-12 22:20:21 +0500

    Refactor love calculator code by commenting out input and calculation logic for future enhancements

--- Projects/8.LoveScore/Lovesocre.py
+++ Projects/8.LoveScore/Lovesocre.py
@@ -18,25 +18,25 @@
 # if __name__ == "__main__":
 #     main()
 
-print("wellcome to the love calculator")
-name1 = input("Enter your name 😎:").lower()
-name2 = input("Enter their name 😍:").lower()
-combine_string=name1+name2
-lettercount_t=combine_string.count("t")
-lettercount_r=combine_string.count("r")
-lettercount_u=combine_string.count("u")
-lettercount_e=combine_string.count("e")
-lettercount_l=combine_string.count("l")
-lettercount_o=combine_string.count("o")
-lettercount_v=combine_string.count("v")
-lettercount_e=combine_string.count("e")
+# print("wellcome to the love calculator")
+# name1 = input("Enter your name 😎:").lower()
+# name2 = input("Enter their name 😍:").lower()
+# combine_string=name1+name2
+# lettercount_t=combine_string.count("t")
+# lettercount_r=combine_string.count("r")
+# lettercount_u=combine_string.count("u")
+# lettercount_e=combine_string.count("e")
+# lettercount_l=combine_string.count("l")
+# lettercount_o=combine_string.count("o")
+# lettercount_v=combine_string.count("v")
+# lettercount_e=combine_string.count("e")
 
-love_count= lettercount_t+lettercount_r+lettercount_u+lettercount_e+lettercount_l+lettercount_o+lettercount_v
+# love_count= lettercount_t+lettercount_r+lettercount_u+lettercount_e+lettercount_l+lettercount_o+lettercount_v
 
-if love_count>=8:
-  print("you botg are in love")
-else:
-    print("you should try another way")
+# if love_count>=8:
+#   print("you botg are in love")
+# else:
+#     print("you should try another way")
   
 # print(f"here is you and your lover {combine_string}")
 # print(f"hello your string count {love_count}")
